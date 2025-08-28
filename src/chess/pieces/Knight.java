bfd51a905aa4c4baa12ab7fb8eb61998e45107b6
gabrielspk <gabrielsilvaferreira007@gmail.com>
2025-08-28 17:42:02 -0300

    refactor(knight): improve knight class readibility

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -7,40 +7,59 @@ import chess.Color;
 
 public class Knight extends ChessPiece {
 
+	private static final int[][] KNIGHT_DIRECTIONS = { { -2, -1 }, // duas cima uma pra esquerda
+			{ -2, 1 }, // duas cima uma pra direita
+			{ 2, -1 }, // duas baixo uma pra esquerda
+			{ 2, 1 }, // duas baixo uma pra direita
+			{ -1, -2 }, // uma cima duas pra esquerda
+			{ -1, 2 }, // uma cima duas pra direita
+			{ 1, -2 }, // uma baixo duas pra esquerda
+			{ 1, 2 }, // uma baixo duas pra direita
+	};
+
 	public Knight(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "H";
 	}
-	
+
+	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
-		int[][] movimentosPossiveis = {
-				{-2, -1}, // duas cima uma pra esquerda
-				{-2, 1}, // duas cima uma pra direita
-				{2, -1}, // duas baixo uma pra esquerda
-				{2, 1}, // duas baixo uma pra direita
-				{-1, -2}, //uma cima duas pra esquerda
-				{-1, 2}, // uma cima duas pra direita
-				{1, -2}, // uma baixo duas pra esquerda
-				{1, 2}, // uma baixo duas pra direita
-		};
-		
-		for (int[] movimento : movimentosPossiveis) {
-			Position targetPosition = new Position(position.getRow() + movimento[0], position.getColumn() + movimento[1]);
-			if (getBoard().positionExists(targetPosition)) {
-				if (!getBoard().thereIsAPiece(targetPosition)) {
-					possibleMoves[targetPosition.getRow()][targetPosition.getColumn()] = true;
-				}
-				else if (isThereOpponentPiece(targetPosition)){
-					possibleMoves[targetPosition.getRow()][targetPosition.getColumn()] = true;
-				}
-			}
+		boolean[][] moves = createEmptyMovesMatrix();
+		addKnightMoves(moves);
+		return moves;
+	}
+
+	private boolean[][] createEmptyMovesMatrix() {
+		return new boolean[getBoard().getRows()][getBoard().getColumns()];
+	}
+
+	private void addKnightMoves(boolean[][] moves) {
+		for (int[] direction : KNIGHT_DIRECTIONS) {
+			markPossibleMoves(moves, direction[0], direction[1]);
 		}
-		return possibleMoves;
+	}
+
+	private void markPossibleMoves(boolean[][] moves, int rowDirection, int columnDirection) {
+		Position target = new Position(position.getRow() + rowDirection, position.getColumn() + columnDirection);
+
+		if (canMoveToEmptySquare(target) || canCaptureOpponentPiece(target)) {
+			markAsValidMove(moves, target);
+		}
+	}
+
+	private boolean canMoveToEmptySquare(Position position) {
+		return getBoard().positionExists(position) && !getBoard().thereIsAPiece(position);
+	}
+
+	private boolean canCaptureOpponentPiece(Position position) {
+		return getBoard().positionExists(position) && isThereOpponentPiece(position);
+	}
+
+	private void markAsValidMove(boolean[][] moves, Position position) {
+		moves[position.getRow()][position.getColumn()] = true;
 	}
 }
